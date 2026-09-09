890968b9eb9c8c4a78eaf4d8a3b636dc860d3ce9
JarrodSeville <jarrodseville403@gmail.com>
2026-09-09 23:03:38 +0100

    Commit-1: User input for difficulty
    Asks the user which difficulty level they would like to play and checks to see if the input is valid. An error message will be displayed if not.

---
+++ number-guessing-game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_guessing_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This asks the user for a difficulty level
+            Console.WriteLine("Select a difficulty level (super easy (1), easy (2), normal (3), hard (4))");
+            string input_difficulty = Console.ReadLine();
+
+            // The if is when the input is valid
+            if (int.TryParse(input_difficulty, out int result_input_difficulty) && result_input_difficulty >= 1 && result_input_difficulty <= 4)
+            {
+               
+            }
+
+            // The else is when the input is invalid
+            else
+            {
+                Console.WriteLine("Invalid input. Please select a valid difficulty level.");
+            }
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
